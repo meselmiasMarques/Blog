8a71af5ba753716464ad3dcb428faab6ec6fc511
Meselmias Marques <meselmias.marques.lima@gmail.com>
2022-10-13 17:52:51 -0300

    importação inicial

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    public class Program
+    {
+        private const string CONNECTION_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Blog;Integrated Security=True;Connect Timeout=30;
+            Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static void Main(string[] args)
+        {
+
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            ReadUsers(connection);
+            ReadRoles(connection);
+
+            connection.Close();
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+
+            foreach (var item in users)
+                Console.WriteLine($"{item.Name}");
+
+        }
+
+        public static void ReadRoles(SqlConnection connection)
+        {
+
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+
+            foreach (var item in roles)
+                Console.WriteLine($"{item.Name}");
+
+        }
+
+    }
+}
---
+++ sql/Blog_Insert_User.sql
@@ -0,0 +1,10 @@
+-- Select rows from a Table or View '[User]' in schema '[dbo]'
+SELECT * FROM [dbo].[Role]
+
+
+-- Insert rows into table 'Role' in schema '[dbo]'
+INSERT INTO [dbo].[Role]
+
+VALUES
+( -- First row: values for the columns in the list above
+'Autor','author')
